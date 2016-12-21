9e5e388886045f9fccf385d5c9d060d0595bc9f2
Shahrin Khan <shahrin@uken.com>
2016-12-20 19:08:53 -0500

    Finish part two.

--- 2016/day19.py
+++ 2016/day19.py
@@ -46,6 +46,43 @@ def part_one():
         elf = elf.next_elf
         iterations += 1
 
+def part_two():
+    first_elf = Elf(1)
+    elf = first_elf
+    before_across_elf = None
+    num_elves = 3014387
+
+    before_across_elf_num = num_elves / 2
+
+    for i in range(1, num_elves):
+        elf.next_elf = Elf(i + 1)
+
+        if i == before_across_elf_num:
+            before_across_elf = elf
+
+        elf = elf.next_elf
+
+    elf.next_elf = first_elf
+    elf = first_elf
+
+    print "Done making elves."
+
+    iterations = 0
+    while num_elves > 0:
+        if iterations % 10000 == 0:
+            print "did", iterations, "iterations"
+            print num_elves, "elves left"
+
+        before_across_elf.next_elf = before_across_elf.next_elf.next_elf
+
+        if num_elves % 2 == 1:
+            before_across_elf = before_across_elf.next_elf
+
+        num_elves -= 1
+        iterations += 1
+
+    print num_elves, "elves left"
+    print "The winner is", before_across_elf.i
 
 if __name__ == '__main__':
-    part_one()
+    part_two()
